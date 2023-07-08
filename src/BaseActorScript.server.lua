7d3f5356857cad838bf8aaaf214e3dcfb7cd41d0
MightyPart <cameron.campbell325@gmail.com>
2023-07-08 14:11:02 +0100

    Update BaseActorScript.server.lua

--- src/BaseActorScript.server.lua
+++ src/BaseActorScript.server.lua
@@ -1,10 +1,20 @@
-local Mod, DoneEvent
+--> [ SERVICES ] -------------------------------------------------------------
+local SharedTableRegistry = game:GetService("SharedTableRegistry")
+------------------------------------------------------------------------------
 
-script.Parent:BindToMessage("ActorPool", function(...)
-	if not Mod then Mod = require(script.Parent:FindFirstChildWhichIsA("ModuleScript")) end
+
+--> [ VARIABLES ] ------------------------------------------------------------
+local Mod, DoneEvent, Guid
+------------------------------------------------------------------------------
+
+
+if script.Parent:IsA("Actor")  then
+	script.Parent:BindToMessageParallel("ActorPool", function()
+	if not Mod then task.synchronize(); Mod = require(script.Parent:FindFirstChildWhichIsA("ModuleScript")); task.desynchronize() end
 	if not DoneEvent then DoneEvent = script.Parent.DoneEvent end
-	task.desynchronize()
-	
-	Mod(...)
+	if not Guid then Guid = script.Parent:GetAttribute("Guid") end
+
+	Mod( SharedTableRegistry:GetSharedTable(`ActorPoolv4.1/{Guid}`) )
 	DoneEvent:Fire()
-end)
+	end)
+end
